4b393abe0d82356c9e545d2109de4105937ff500
hypark <hypark.thee@gmail.com>
2020-11-11 01:22:19 +0900

    Memo Class

---
+++ SimpleMemo/Model/Model.swift
@@ -0,0 +1,23 @@
+//
+//  Model.swift
+//  SimpleMemo
+//
+//  Created by Alex Young on 2020/11/11.
+//  Copyright © 2020 HayoungPark. All rights reserved.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "Lorem Ipsum")
+    ]
+}
