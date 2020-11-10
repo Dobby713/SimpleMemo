9ce260a203f69bf4c6986879ba4fa6d60df11449
hypark <hypark.thee@gmail.com>
2020-11-11 00:44:34 +0900

    Initial Commit

---
+++ SimpleMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleMemo
+//
+//  Created by Alex Young on 2020/11/11.
+//  Copyright © 2020 HayoungPark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
