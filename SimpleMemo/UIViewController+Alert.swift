4f9289750fb7854ca0c1061ad28c84494da398f8
hypark <hypark.thee@gmail.com>
2020-11-11 22:39:43 +0900

    메모 목록 업데이트

---
+++ SimpleMemo/UIViewController+Alert.swift
@@ -0,0 +1,18 @@
+//
+//  UIViewController+Alert.swift
+//  SimpleMemo
+//
+//  Created by Alex Young on 2020/11/11.
+//  Copyright © 2020 HayoungPark. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func alert(title: String = "알림", message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+}
